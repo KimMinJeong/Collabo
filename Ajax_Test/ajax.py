492abc6e127b6c049899bf72c7dc6a01e5e3f850
KIM <zooey@spoqa.com>
2014-01-16 17:15:23 +0900

    부트스트랩_flask

--- Ajax_Test/ajax.py
+++ Ajax_Test/ajax.py
@@ -1,13 +1,40 @@
 from flask import Flask, jsonify, render_template, request
+from sqlalchemy import String , Integer, Sequence,Column
+from sqlalchemy import create_engine
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from sqlalchemy.ext.declarative import declarative_base
+
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS' ,silent=True)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost/pos'
 
-@app.route('/_add_numbers')
-def add_numbers():
-    """Add two numbers server side, ridiculous but well..."""
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+db = SQLAlchemy(app)
 
+class info(db.Model):
+    __tablename__ ='info'
+    id = db.Column('info_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
+    text = db.Column(db.String(30))
+    
+    def __init__(self, title, text):
+        self.title= title
+        self.text = text
+    def __repr(self):
+        return '<info %s %s>' % self.title ,self.text
+
+db.create_all()
+db.session.commit()
+
+a = db.session.info.query.all()
+print a
+
+@app.route('/get_db', methods=['GET']) 
+def get_db():
+    db = info.query.filter_by(title='Kim').first()
+    return db
 
 @app.route('/')
 def index():
