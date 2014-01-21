81784d6a48d50b721c9ee168979904d913b35229
Areum Kim <anny@spoqa.com>
2014-01-21 14:24:55 +0900

    upload bootstrap_main_contents test

---
+++ Board/Board_main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, g, session, flash, \
+     redirect, url_for, abort, jsonify
+from flask_openid import OpenID
+from openid.extensions import pape
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from os.path import dirname, join
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1111@localhost:5432/ForFlask'
+db = SQLAlchemy(app)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+#oid = OpenID(app, join(dirname(__file__), 'openid_store'))
+
+@app.route('/')
+def index():    
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return 1
+
+@app.route('/ex')
+def contents():
+    return render_template('contents.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
