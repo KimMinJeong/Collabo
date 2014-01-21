db8f63a9ac567b5ffcdd7a64acfc2d93fc2eead8
Areum Kim <anny@spoqa.com>
2014-01-21 18:33:20 +0900

    last update 1/21

--- Board/Board_main.py
+++ Board/Board_main.py
@@ -1,32 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, render_template, request, g, session, flash, \
-     redirect, url_for, abort, jsonify
-from flask_openid import OpenID
-from os import environ
-from openid.extensions import pape
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from os.path import dirname, join
-from flask.ext.sqlalchemy import SQLAlchemy
-
-
-
-app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1111@localhost:5432/ForFlask'
-oid = OpenID(app, join(dirname(__file__), 'openid_store'))
-app.config.update(
-        DATABASE_URI = 'postgresql://postgres:1111@localhost:5432/album',
-        SECRET_KEY = 'development key',
-        DEBUG = True
-    )
-
-db = SQLAlchemy(app)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-
-
+from config import *
 
 # def get_user():
 #    return g.db.get('oid-' + session.get('openid', ''))
@@ -38,9 +11,7 @@ def index():
 @app.before_request
 def before_request():
     if not 'openid' in session:
-        flash(u'You have some problem..')
-    else:
-        flash(u'You logged in')
+        flash(u'You have some problem..')    
 
    
     
--- Board/config.py
+++ Board/config.py
@@ -1,32 +1,27 @@
-# -*- coding: utf-8 -*-
-
 from flask import Flask, render_template, request, g, session, flash, \
-     redirect, url_for, abort
+     redirect, url_for, abort, jsonify
 from flask_openid import OpenID
+from os import environ
 from openid.extensions import pape
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from os.path import dirname, join
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1111@localhost:5432/ForFlask'
+oid = OpenID(app, join(dirname(__file__), 'openid_store'))
 app.config.update(
         DATABASE_URI = 'postgresql://postgres:1111@localhost:5432/album',
         SECRET_KEY = 'development key',
         DEBUG = True
     )
-"""openid 설치"""
-oid = OpenID(app, join(dirname(__file__), 'openid_store'))
 
-engine = create_engine(app.config['DATABASE_URI'])
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
-print Base.query
-def init_db():
-    Base.metadata.create_all(bind=engine)
+db = SQLAlchemy(app)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 
