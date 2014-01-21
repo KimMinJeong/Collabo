8ecba6b3c68cb7d3e18a424368e1d25c5d42761f
KIM <zooey@spoqa.com>
2014-01-21 15:49:11 +0900

    add plus

--- Board/Board_List.py
+++ Board/Board_List.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify, render_template, request
 from sqlalchemy import String , Integer, Sequence,Column
 from sqlalchemy import create_engine
-from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost/pos'
 
-db = SQLAlchemy(app)
+
 
 @app.route('/')
-def index():
-    return render_template('layout.html')
+def top():
+    return render_template('top.html')
 
-@app.route('/login')
-def login():
-    return render_template('index.html')
- 
+@app.route('/board_list')
+def board_list():
+    return render_template('board_list.html')
+
+@app.route('/board_insert')
+def board_insert():
+    return render_template('board_insert.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT',5000)))
