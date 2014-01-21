af94f252bbb7c8bd51277ec930e7e878aae61b79
KIM <zooey@spoqa.com>
2014-01-21 10:12:20 +0900

    부트스트랩 올림

--- Board/Board_List.py
+++ Board/Board_List.py
@@ -1,2 +1,22 @@
-from flask import Flask 
-from sqlalchemy import String, Sequence, Column
+from flask import Flask, jsonify, render_template, request
+from sqlalchemy import String , Integer, Sequence,Column
+from sqlalchemy import create_engine
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost/pos'
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+@app.route('/login')
+def login():
+    return render_template('index.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
