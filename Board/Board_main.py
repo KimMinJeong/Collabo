e9b2c7861c5467674a1ca0f482d2470203ee97d4
Areum Kim <anny@spoqa.com>
2014-01-21 15:49:08 +0900

    update

--- Board/Board_main.py
+++ Board/Board_main.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, g, session, flash, \
      redirect, url_for, abort, jsonify
 from flask_openid import OpenID
+from os import environ
 from openid.extensions import pape
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -29,4 +30,4 @@ def contents():
     return render_template('contents.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(environ.get('PORT',5000)))
