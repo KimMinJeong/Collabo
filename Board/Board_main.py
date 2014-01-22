8e2c297481172e4e9c47760703807a98b55f2421
Areum Kim <anny@spoqa.com>
2014-01-22 13:02:41 +0900

    function 1 complete (gravatar...)

--- Board/Board_main.py
+++ Board/Board_main.py
@@ -8,46 +8,58 @@ from config import *
 def index():      
     return render_template('index.html')
 
-@app.before_request
-def before_request():
-    if not 'openid' in session:
-        flash(u'You have some problem..')    
+#@app.before_request
+#def before_request():
+# if not 'id' in session:
+#     flash(u'LogIN')    
 
    
     
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 @oid.loginhandler
 def login():
-    # if we are already logged in, go back to were we came from
-    #if session.get('openid'):
-    #    return redirect(oid.get_next_url())
-    if request.method == 'POST':
-        openid = request.form.get('openid')
-        
-        if openid:
-            pape_req = pape.Request([]) #what is pape_request???
-            return oid.try_login("https://www.google.com/accounts/o8/id",
-            ask_for=['email', 'fullname', 'nickname'])
-            
-                                   
-    return render_template('login.html', next=oid.get_next_url(),
+   #로그인을 하게 되면 바로 google 창이 뜰꺼야.
+    pape_req = pape.Request([]) #what is pape_request???
+    return oid.try_login("https://www.google.com/accounts/o8/id",
+    ask_for=['email', 'fullname', 'nickname'])
+                                      
+    return render_template('example.html', next=oid.get_next_url(),
                            error=oid.fetch_error())
+
 
 @oid.after_login
 def after_login(resp):
-    session['openid'] = resp.identity_url
-    if not session.get('openid'):
+    session['id'] = resp.identity_url
+    if not session.get('id'):
         return redirect(oid.get_next_url())
-    return redirect(url_for('post', next=oid.get_next_url(),
+    g.email = resp.email
+    #그라바타 url이랑 email주소 리턴!
+    gravatar = set_img(resp)    
+    return redirect(url_for('contents', next=oid.get_next_url(),
                             name=resp.fullname or resp.nickname,
-                            email=resp.email))
-
+                            email=resp.email, gravata_url=gravatar[0],
+                            email_gra=gravatar[1]))
+    
+    
+def set_img(resp):
+   
+    email_gra = resp.email
+    size = 40
+    gravatar_url = "http://www.gravatar.com/avatar/" + \
+                    hashlib.md5(email_gra.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode( {'d': 'mm' ,'s': str(size)} )     
+    return gravatar_url, email_gra     
+      
+      
 @app.route('/post')
 def post():
     return render_template('example.html')
+
+
 @app.route('/contents')
 def contents():
     return render_template('contents.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT',5000)))
--- Board/config.py
+++ Board/config.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request, g, session, flash, \
-     redirect, url_for, abort, jsonify
-from flask_openid import OpenID
-from os import environ
-from openid.extensions import pape
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from os.path import dirname, join
+from flask import Flask, render_template, request, g, session, flash, redirect, \
+    url_for, abort, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask_openid import OpenID
+from openid.extensions import pape
+from os import environ
+from os.path import dirname, join
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+import urllib
+import hashlib
 
 
 app = Flask(__name__)
