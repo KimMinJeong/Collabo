ef30ea89dc2b49d590215718d2421b37dc7644b2
KIM <zooey@spoqa.com>
2014-02-05 15:31:14 +0900

    add glyphicons.png , Add Column "section" in Comments for Admin , Add Summernote Editor

--- Board/Board_main.py
+++ Board/Board_main.py
@@ -38,46 +38,56 @@ class User(db.Model):
     email = db.Column(String(60))
     posts = db.relationship('Post', backref='author', \
                             cascade="all, delete-orphan", passive_deletes=True)
-    comments = db.relationship('Comment', backref='author', \
+    comments = db.relationship('Comment', backref='user', \
                                cascade="all, delete-orphan", passive_deletes=True)
-
+    
     def __init__(self,name,email):
         self.name = name
         self.email = email
-        
+
+    def is_admin(self):
+        return self.email in (
+            'jc@spoqa.com',
+            'grant@spoqa.com',
+            'richard@spoqa.com',
+            'zooey@spoqa.com'
+        )
+
     def __repr__(self):
         return "<User id={0!r}, name={1!r}, email={2!r}>".\
                 format(self.id, self.name, self.email)
+                
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text, nullable=True)
-    #opinion = db.Column(db.Text, nullable=False)
+    #opinion = db.Column(db.Text, dullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='cascade'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id',ondelete='cascade'))
+    section = db.Column(db.Boolean)
     created_at = db.Column(DateTime(timezone=True), nullable=False,
                                      default=functions.now())
-    
-    def __init__(self, comment, user_id, post_id):
+
+    def __init__(self, comment, user_id, post_id, section):
         self.comment = comment
-        #self.opinion = opinion
         self.user_id = user_id
         self.post_id = post_id
+        self.section = section
 
     def __repr__(self):
-        return "<Comment id={0!r},comment={1!r}, user_id={2!r}, post_id={3!r}>".\
-                format(self.id, self.comment, self.user_id, self.post_id)
+        return "<Comment id={0!r},comment={1!r}, user_id={2!r}, post_id={3!r}, section={4!r}>".\
+                format(self.id, self.comment, self.user_id, self.post_id, self.section)
 
-
+    
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(Integer, primary_key=True)
     category = db.Column(db.String(10))
     subject = db.Column(db.String(50))
     status = db.Column(db.String(20))
-    contents = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='cascade'))
     created_at = db.Column(DateTime(timezone=True),
                            nullable=False,
@@ -85,16 +95,16 @@ class Post(db.Model):
     comments = db.relationship('Comment', backref='post', \
                                cascade="all, delete-orphan", passive_deletes=True)
     
-    def __init__(self, category, subject, status, contents, user_id):
+    def __init__(self, category, subject, status, content, user_id):
         self.category = category
         self.subject = subject
         self.status = status
-        self.contents = contents
+        self.content = content
         self.user_id = user_id
         
     def __repr__(self):
-        return "<Post id={0!r}, category={1!r}, subject={2!r}, status={3!r}, contents={4!r}> user_id={5!r}".\
-                format(self.id, self.category, self.subject, self.status, self.contents, self.user_id)
+        return "<Post id={0!r}, category={1!r}, subject={2!r}, status={3!r}, content={4!r}> user_id={5!r}".\
+                format(self.id, self.category, self.subject, self.status, self.content, self.user_id)
 
 
 def init_db():
@@ -115,7 +125,6 @@ def login():
         
 @app.route('/posts/lists')
 def board_list():
-    
     post_list = Post.query.all()
     return render_template('board_list.html', post_list=post_list)
 
@@ -125,7 +134,6 @@ def show(id):
     post= db.session.query(Post).get(id)
     comm_list = db.session.query(Comment).filter(Comment.post_id==id).all()
     
-    
     return render_template('contents.html',
                             post=post, comm_list=comm_list)
 
@@ -134,19 +142,24 @@ def show(id):
 def put_post(id):
     post = Post.query.get(id)
     post.status = request.values.get('status')
-    post.comments.append(Comment( comment=request.form['opinion'],
-                                 user_id=session.get('user_id'),
-                                 post_id=session.get('post_id')
+    if User.is_admin():
+        section = True
+    else:
+        section = False
+    post.comments.append(Comment(comment=request.form['opinion'],
+                                 user_id=session['user']['id'],
+                                 post_id=session.get('post_id'),
+                                 section = section 
                                 ))
-
+    
     db.session.commit()
     return redirect(oid.get_next_url())
 
 
 @app.route('/posts/status', methods=['GET'])
 def board_detail():
-    post = Post.query.all()
-    return render_template('board_detail.html', post=post)
+    post_list = Post.query.get(id)
+    return render_template('board_detail.html', post_list=post_list)
 
 
 @app.route('/register', methods=['GET'])
@@ -196,9 +209,9 @@ def board_insert():
     category = request.form["category"]
     subject = request.form["subject"]
     status = request.form["status"]
-    contents = request.form["contents"]   
+    content = request.form["content"]   
     user_id = session['user']['id']
-    db_insert = Post(category, subject, status, contents, user_id)
+    db_insert = Post(category, subject, status, content, user_id)
     db.session.add(db_insert)
     db.session.commit()
     return redirect(url_for('board_list'))
@@ -215,7 +228,17 @@ def add_comm(id):#comment 추가
         user_id = session['user']['id']
         comment = request.form['reply']
         post_id = id
-        comment = Comment(comment, user_id, post_id)
+    if session['user']['email'] in (
+                                    'jc@spoqa.com',
+                                    'grant@spoqa.com',
+                                    'richard@spoqa.com',
+                                    'zooey@spoqa.com'
+                                    ):
+        section = 10
+    else:
+        section = 99
+        
+        comment = Comment(comment, user_id, post_id, section)
         db.session.add(comment)       
         db.session.commit()
     return redirect(oid.get_next_url())
