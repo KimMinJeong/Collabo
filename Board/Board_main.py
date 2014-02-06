f1662a9a63b651a5b2707b13c511234cff2c228c
KIM <zooey@spoqa.com>
2014-02-06 10:12:54 +0900

    Delete unused file, Add is_admin() in User Class, Modify user dictation --> before_request(g.user)

--- Board/Board_main.py
+++ Board/Board_main.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_openid import OpenID
 from openid.extensions import pape
 from os import environ
+from functools import wraps
 from os.path import dirname, join
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,7 +16,7 @@ from sqlalchemy.sql.expression import case
 from sqlalchemy.types import DateTime, Boolean
 import hashlib
 import urllib
-import os
+import os, json
 
 
 app = Flask(__name__)
@@ -34,16 +35,18 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(Integer, primary_key=True)
-    name = db.Column(String(60))
-    email = db.Column(String(60))
+    name = db.Column(db.String(60))
+    email = db.Column(db.String(60))
     posts = db.relationship('Post', backref='author', \
                             cascade="all, delete-orphan", passive_deletes=True)
     comments = db.relationship('Comment', backref='user', \
                                cascade="all, delete-orphan", passive_deletes=True)
+    authority = db.Column(db.Boolean)
     
-    def __init__(self,name,email):
+    def __init__(self, name, email, authority):
         self.name = name
         self.email = email
+        self.authority = authority
 
     def is_admin(self):
         return self.email in (
@@ -52,21 +55,19 @@ class User(db.Model):
             'richard@spoqa.com',
             'zooey@spoqa.com'
         )
-
+        
     def __repr__(self):
-        return "<User id={0!r}, name={1!r}, email={2!r}>".\
-                format(self.id, self.name, self.email)
+        return "<User id={0!r}, name={1!r}, email={2!r}, authority={3!r}>".\
+                format(self.id, self.name, self.email, self.authority)
                 
-
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text, nullable=True)
-    #opinion = db.Column(db.Text, dullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='cascade'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id',ondelete='cascade'))
-    section = db.Column(db.Boolean)
+    section = db.Column(db.Integer)
     created_at = db.Column(DateTime(timezone=True), nullable=False,
                                      default=functions.now())
 
@@ -75,7 +76,7 @@ class Comment(db.Model):
         self.user_id = user_id
         self.post_id = post_id
         self.section = section
-
+        
     def __repr__(self):
         return "<Comment id={0!r},comment={1!r}, user_id={2!r}, post_id={3!r}, section={4!r}>".\
                 format(self.id, self.comment, self.user_id, self.post_id, self.section)
@@ -88,7 +89,7 @@ class Post(db.Model):
     subject = db.Column(db.String(50))
     status = db.Column(db.String(20))
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='cascade'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'))
     created_at = db.Column(DateTime(timezone=True),
                            nullable=False,
                            default=functions.now())
@@ -106,22 +107,36 @@ class Post(db.Model):
         return "<Post id={0!r}, category={1!r}, subject={2!r}, status={3!r}, content={4!r}> user_id={5!r}".\
                 format(self.id, self.category, self.subject, self.status, self.content, self.user_id)
 
-
 def init_db():
     db.create_all()
 
 
-@app.route('/')
+@app.before_request
+def before_request():
+    if not session.get('user_email') is None:
+        g.user = User.query.filter_by(email=session.get('user_email')).first()
+        
+
+@app.route('/', methods=['GET'])
 def index():      
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user_email') is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/login', methods=['GET','POST'])
+@oid.loginhandler  
 def login():
     return oid.try_login("https://www.google.com/accounts/o8/id",
           ask_for=['email','fullname','nickname'])
-        
+      
         
 @app.route('/posts/lists')
 def board_list():
@@ -132,24 +147,22 @@ def board_list():
 @app.route('/posts/<int:id>', methods=['GET'])
 def show(id):    
     post= db.session.query(Post).get(id)
-    comm_list = db.session.query(Comment).filter(Comment.post_id==id).all()
+    comm_list_admin= Comment.query.filter(Comment.post_id==id,Comment.section==10).all()
+    comm_list = Comment.query.filter(Comment.post_id==id, Comment.section==99).all()
     
     return render_template('contents.html',
-                            post=post, comm_list=comm_list)
+                            post=post, comm_list_admin=comm_list_admin, comm_list=comm_list)
 
 
 @app.route('/posts/<int:id>', methods=['POST'])
 def put_post(id):
     post = Post.query.get(id)
     post.status = request.values.get('status')
-    if User.is_admin():
-        section = True
-    else:
-        section = False
+    section = 10
     post.comments.append(Comment(comment=request.form['opinion'],
-                                 user_id=session['user']['id'],
+                                 user_id=g.user.id,
                                  post_id=session.get('post_id'),
-                                 section = section 
+                                 section=section 
                                 ))
     
     db.session.commit()
@@ -171,7 +184,8 @@ def register():
 def add_user():
     email = request.form['newEmail']
     name = email.split('@')
-    user = User(name[0],email)
+    authority = False
+    user = User(name[0],email,authority)
     db.session.add(user)
     db.session.commit()
     flash(u'추가!')
@@ -180,18 +194,14 @@ def add_user():
 
 @oid.after_login
 def after_login(resp):
-    gravatar = set_img(resp.email)     
-    user = User.query.filter_by(email=resp.email).first()
-    user = { "id" : user.id,
-              "name" : user.name,
-              "email" : user.email
-            }
-    if not user:
-        return redirect(oid.get_next_url()) 
-    flash(u'Successfully signed in')   
-    session['gravatar'] = gravatar       
-    session['user'] = user
-    return redirect(url_for('board_list'))
+    session['user_email'] = resp.email
+    
+    if session.get('user_email') is None:
+        return redirect(oid.get_next_url())
+    else:
+        gravatar = set_img(resp.email)    
+        session['gravatar'] = gravatar              
+        return redirect(url_for('board_list'))
 
 
 def set_img(s):
@@ -210,7 +220,7 @@ def board_insert():
     subject = request.form["subject"]
     status = request.form["status"]
     content = request.form["content"]   
-    user_id = session['user']['id']
+    user_id = g.user.id
     db_insert = Post(category, subject, status, content, user_id)
     db.session.add(db_insert)
     db.session.commit()
@@ -225,17 +235,9 @@ def board_get():
 @app.route('/posts/<int:id>/comment', methods=['POST'])
 def add_comm(id):#comment 추가
     if request.method =='POST':
-        user_id = session['user']['id']
+        user_id = g.user.id
         comment = request.form['reply']
         post_id = id
-    if session['user']['email'] in (
-                                    'jc@spoqa.com',
-                                    'grant@spoqa.com',
-                                    'richard@spoqa.com',
-                                    'zooey@spoqa.com'
-                                    ):
-        section = 10
-    else:
         section = 99
         
         comment = Comment(comment, user_id, post_id, section)
@@ -273,5 +275,4 @@ def edit():
 
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT',5000)))
